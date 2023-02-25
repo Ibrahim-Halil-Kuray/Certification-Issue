b3f99a833e5388c548e27f97597cb5d1232f0441
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-02-25 14:13:19 +0100

    tailwind css configurations done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,9 @@ import img from './Certificate.jpg'
 function App() {
   return (
     <main className="App">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <Certificate pdf={pdf} img={img}/>
     </main>
   );
