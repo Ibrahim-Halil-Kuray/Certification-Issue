713fe58541be9e9ac775ebcfd17b8fdbc3d50506
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-03-13 14:42:50 +0100

    controller.js created

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router =require('./router/router')
+const mongoose=require('mongoose')
+const cors=require('cors')
+
+const app = express();
+
+app.use(express.json());
+
+// Enable Cors
+app.use(cors());
+
+// DB Config
+const db = "mongodb+srv://ibrahim:nyWJIzia1FxJhorx@participants.tqlnkll.mongodb.net/Certificate?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+  
+const PORT = process.env.PORT || 8081;
+
+
+app.use("/api/items", router)
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
